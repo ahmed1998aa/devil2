fcc16e3c0a16f08f97a80826cae8960c1949e07a
ahmed1998aa <30708899+ahmed1998aa@users.noreply.github.com>
2017-09-09 16:18:32 -0700

    Update TSHAKE.sh

--- TSHAKE.sh
+++ TSHAKE.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-token="التوكن هنا"
+token="420405135:AAENckG2syNWZ4RuCFHjQxaqByC7eNaBi7U"
 function print_logo() {
 	echo -e "\e[38;5;77m"   
 echo -e "       CH > @TshAkETEAM            "
